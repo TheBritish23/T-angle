7e26e9842d044674315bfb548fefe14cf16de5e9
Soro Cat <sorousha326@gmail.com>
2020-03-30 11:40:17 -0700

    uglify again

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');  
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -11,6 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+     new UglifyJsPlugin(), 
      new CleanWebpackPlugin(),
      new HtmlWebpackPlugin({
        title: 'T-angle',
