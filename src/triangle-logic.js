47845ba4187f028d43c2ca79292a784710c3e020
Soro Cat <sorousha326@gmail.com>
2020-04-02 10:31:30 -0700

    add extra to triangles

--- src/triangle-logic.js
+++ src/triangle-logic.js
@@ -1,8 +1,9 @@
 function Triangle(side1, side2, side3) {
   var Triangle = require('./../src/triangle-logic.js').triangleModule;
   export function Triangle(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
   }
-  export function Triangle() {
-}
 exports.triangleModule = Triangle;
 }
