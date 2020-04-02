2e54d93070443f6edb2bc53cdf961caedc657a35
Soro Cat <sorousha326@gmail.com>
2020-04-02 09:37:11 -0700

    add changes

--- src/triangle-logic.js
+++ src/triangle-logic.js
@@ -2,5 +2,7 @@ function Triangle(side1, side2, side3) {
   var Triangle = require('./../src/triangle-logic.js').triangleModule;
   export function Triangle(side1, side2, side3) {
   }
+  export function Triangle() {
 }
 exports.triangleModule = Triangle;
+}
