43bd355bce06b3c151b34678a1c00342f85282f7
Soro Cat <sorousha326@gmail.com>
2020-04-02 11:34:19 -0700

    add changes

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -8,9 +8,11 @@ Triangle.prototype.checkType = function() {
 
   test('should correctly create a triangle object with three lengths', () => {
     var triangle = new Triangle(2,4,5);
+    var scalTriangle = new Triangle(4,5,7)
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
     expect(value1).toEqual(value2);
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
   });
 });
