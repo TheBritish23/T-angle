ca3a0ee3e880596b5d1b605170586c6a8be2667c
Soro Cat <sorousha326@gmail.com>
2020-04-03 12:00:17 -0700

    add console and debugger

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -5,6 +5,8 @@ describe('Triangle', () => {
   test('should correctly create a triangle object with three lengths', () => {
     var triangle = new Triangle(2,4,5);
     var scalTriangle = new Triangle(4,5,7)
+    console.log(triangle);
+    debugger;
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
