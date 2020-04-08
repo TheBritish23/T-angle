c4858e9612257a3da1105f4bd253c8cae2bbf302
Soro Cat <sorousha326@gmail.com>
2020-04-08 09:49:24 -0700

    add ending mark

--- src/triangle-logic.js
+++ src/triangle-logic.js
@@ -1,5 +1,5 @@
   export function Triangle(side1, side2, side3){
-function Triangle(side1, side2, side3);
+function Triangle(side1, side2, side3){
 
   var Triangle = require('./../src/triangle-logic.js').triangleModule;
 
@@ -8,7 +8,8 @@ function Triangle(side1, side2, side3);
     this.side2 = side2;
     this.side3 = side3;
     debugger;
-  };
+  }
+ };
 
   Triangle.prototype.checkType = function() {
     if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
